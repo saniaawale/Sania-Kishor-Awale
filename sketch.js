f7230054b2f97a7af55f2c12bc39680898fd86e2
Sania Awale <ska7008@ad.nyu.edu>
2023-04-28 12:00:00 -0400

    first commit

---
+++ sketch.js
@@ -0,0 +1,69 @@
+function setup() {
+  createCanvas(600, 600);
+  
+}
+
+let snake;
+let s = 20;
+
+class Snake {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.xspeed = 1;
+    this.yspeed = 0;
+    this.total = 0;
+    this.tail = [];
+  }
+
+  eat(posfood) {
+    let d = dist(this.x, this.y, posfood.x, posfood.y);
+    if (d < 1) {
+      this.total++;
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  dir(x, y) {
+    this.xspeed = x;
+    this.yspeed = y;
+  }
+
+  death() {
+    for (let i = 0; i < this.tail.length; i++) {
+      let pos = this.tail[i];
+      let d = dist(this.x, this.y, pos.x, pos.y);
+      if (d < 1) {
+        this.total = 0;
+        this.tail = [];
+      }
+    }
+  }
+
+  update() {
+    if (this.total === this.tail.length) {
+      for (let i = 0; i < this.tail.length - 1; i++) {
+        this.tail[i] = this.tail[i + 1];
+      }
+    }
+    this.tail[this.total - 1] = createVector(this.x, this.y);
+
+    this.x = this.x + this.xspeed * s;
+    this.y = this.y + this.yspeed * s;
+
+  }
+
+  show() {
+    fill(255);
+    for (let i = 0; i < this.tail.length; i++) {
+      rect(this.tail[i].x, this.tail[i].y, s, s);
+    }
+    rect(this.x, this.y, s, s);
+  }
+}
+
+function draw() {
+  background(220);
+}
